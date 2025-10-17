aba5e91b409f42fc533d190eb2ef5389585bba41
Injamhossan <injamhossanmamun@gmail.com>
2025-10-18 00:10:44 +0600

    Start project

---
+++ src/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <p className='text-black'>Injam Hossan mamun</p>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import Root from '../Root/Root';
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+        {
+            index:true,
+        }
+    ]
+},
+]);
+
